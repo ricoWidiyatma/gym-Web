1e527b5a0567e03eb7b905308d5010171d230cee
FahrezaAja <146537816+FahrezaAja@users.noreply.github.com>
2024-12-19 00:47:47 +0700

    Update home.php
    ubah tampilan pada navigasi

--- login/home.php
+++ login/home.php
@@ -44,19 +44,13 @@ if (!isset($_SESSION['username'])) {
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#home">Home</a>
+                            <a class="nav-link" aria-current="page" href="home.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#services">services</a>
+                            <a class="nav-link" aria-current="page" href="#about-us">About Us</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#about">about us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#projects">projects</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#contact">contact</a>
+                            <a class="nav-link" href="#contact">Contact</a>
                         </li>
                         <li class="nav-item">
                             <div class="dropdown">
@@ -79,6 +73,10 @@ if (!isset($_SESSION['username'])) {
                                             $res_email = $result['email'];
                                             $res_id = $result['id'];
                                         }
+
+
+                                        echo "<a class='dropdown-item' href='edit.php?id=$res_id'>Change Profile</a>";
+
 
                                         ?>
 
@@ -113,18 +111,79 @@ if (!isset($_SESSION['username'])) {
             <div class="row">
                 <div class="col-lg-4 col-md-12 col-sm-12 text-content">
                     <h1>Welcome to Gatot-Gym</h1>
-                    <p> Kehidupan yang layak di dasari dari tubuh yang sehat juga
+                    <p> Stay Healthy and Always Feeling Strong
                     </p>
                 </div>
                 <div class="col-lg-8 col-md-12 col-sm-12">
-                    <img src="images/download.jpg" alt="" class="img-fluid">
+                    <img src="images/backgatot.jpg" alt="" class="img-fluid">
                 </div>
 
             </div>
         </div>
     </section>
+<!-- package section -->
+<section id="packages" class="package-section py-5">
+    <div class="container">
+        <h2 class="text-center mb-4">Choose Your Membership</h2>
+        <div class="row">
+            <!-- Package 1 -->
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100 text-center">
+                    <div class="card-body">
+                        <h5 class="card-title">Basic</h5>
+                        <p class="card-text">Join Gatot Membership for 1 Month</p>
+                        <p class="card-text fw-bold">Rp 100.000</p>
+                        <a href="#" class="btn btn-primary">Choose</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Package 2 -->
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100 text-center">
+                    <div class="card-body">
+                        <h5 class="card-title">Standard</h5>
+                        <p class="card-text">Join Gatot Membership for 2 Month</p>
+                        <p class="card-text fw-bold">Rp 180.000</p>
+                        <a href="#" class="btn btn-primary">Choose</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Package 3 -->
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100 text-center">
+                    <div class="card-body">
+                        <h5 class="card-title">Premium</h5>
+                        <p class="card-text">Join Gatot Membership for 1 Month and Get a Personal Trainer</p>
+                        <p class="card-text fw-bold">Rp 500.000</p>
+                        <a href="#" class="btn btn-primary">Choose</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
-
+<!-- about us  -->
+<div class="container">
+    <div class="row" id="aboutandcontact">
+        <div class="col-lg-12 col-md-12 col-sm-12"id="about-us">
+            <h1>About Us</h1>
+            <p>Gatot Gym is a fitness center that provides various facilities and equipment to help you achieve your fitness goals. We have a team of experienced and friendly trainers.</p>
+            <p>We offer a range of fitness programs, including weight training, cardio, and yoga. We also have facilities such as changing rooms, showers, and ample parking.</p>
+            <p>At Gatot Gym, we are committed to helping you achieve your fitness goals and improving your quality of life. We believe that fitness is the key to a healthy and happy life.</p>
+            <p>Address : Yogyakarta, Bantul, xxx, jl.xxx no.xxx</p>
+        </div>
+        <div class="col-lg-12 col-md-12 col-sm-12" id="contact">
+            <h1>Contact</h1>
+            <ul>
+                <li><i class="fa fa-phone"></i> Phone Number: 0812-345-678-90</li>
+                <li><i class="fa fa-whatsapp"></i> WhatsApp Number: 0812-345-678-90</li>
+                <li><i class="fa fa-instagram"></i> Instagram: @gatotgym</li>
+            </ul>
+        </div>
+    </div>
+</div>
+    
     <!-- footer section  -->
 
     <footer>
@@ -133,18 +192,14 @@ if (!isset($_SESSION['username'])) {
                 <div class="col-lg-3 col-md-12 col-sm-12">
                     <p class="logo"><i class="bi bi-chat"></i>Gatot Gym</p>
                 </div>
-                <div class="col-lg-6 col-md-12 col-sm-12">
+                <div class="col-lg-6 col-md-6 col-sm-6">
                     <ul class="d-flex">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">services</a></li>
-                        <li><a href="#">projects</a></li>
-                        <li><a href="#">about us</a></li>
-                        <li><a href="#">contact</a></li>
+                        
                     </ul>
                 </div>
 
                 <div class="col-lg-2 col-md-12 col-sm-12">
-                    <p>&copy;2023_BragSpot</p>
+                    <p>&copy;2023 Gatot Gym</p>
                 </div>
 
                 <div class="col-lg-1 col-md-12 col-sm-12">
